6599a56ebec42295e0d4255c95ac26b4a709e036
ybeom <ybeom.me@gmail.com>
2022-12-24 18:47:15 +0900

    [feat] default layout setting

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,11 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 
+import { App } from '@/App';
 import { Home } from '@/pages';
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<App />}>
+        <Route path="/" element={<Home />} />
+      </Route>
     </Routes>
   );
 }
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -6,7 +6,9 @@ export const theme = {
     headline: '#094067',
     paragraph: '#5F6C7B',
     primary: '#3DA9FC',
+    gray100: '#F5F5F5',
     gray200: '#E5E8EB',
     gray300: '#D1D6DB',
+    black: '#000000',
   },
 };
