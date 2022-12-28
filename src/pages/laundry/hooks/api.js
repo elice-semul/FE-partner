01d6440a0727fc0b8b0efafb68d4442267d15c19
ybeom <ybeom.me@gmail.com>
2022-12-28 16:19:03 +0900

    [feat] laundry page orders API 연계

--- src/pages/laundry/hooks/api.js
+++ src/pages/laundry/hooks/api.js
@@ -1,11 +1,22 @@
 import { axiosAuthApi } from '@/utils/apiInstance';
 const BASE_URL = import.meta.env.VITE_BASE;
 
-export const getAllLaundryOrdersApi = async () => {
+export const getAllLaundryOrdersApi = async ({ laundryId }) => {
   try {
     const token = sessionStorage.getItem('Authorization');
     const authInstance = axiosAuthApi(BASE_URL, token);
-    const { data } = await authInstance.get('/orders');
+    const { data } = await authInstance.get(`/orders?laundryId=${laundryId}`, { laundryId });
+    return data;
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+export const getCurrentLaundryApi = async () => {
+  try {
+    const token = sessionStorage.getItem('Authorization');
+    const authInstance = axiosAuthApi(BASE_URL, token);
+    const { data } = await authInstance.get('/partners');
     return data;
   } catch (err) {
     throw new Error(err);
